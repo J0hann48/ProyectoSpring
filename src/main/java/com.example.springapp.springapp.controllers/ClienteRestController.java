24943edfe36641909249b5f288af3cc0638658a1
J0hann48 <joahnn.1996@gmail.com>
2019-07-25 21:26:40 -0500

    Control de excepciones desde backend Spring

--- src/main/java/com.example.springapp.springapp.controllers/ClienteRestController.java
+++ src/main/java/com.example.springapp.springapp.controllers/ClienteRestController.java
@@ -3,10 +3,14 @@ package com.example.springapp.springapp.controllers;
 import com.example.springapp.springapp.models.entity.Cliente;
 import com.example.springapp.springapp.models.service.IClienteService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataAccessException;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 @CrossOrigin(origins = {"http://localhost:4200"})
 @RestController
@@ -15,23 +19,36 @@ public class ClienteRestController {
     @Autowired
     private IClienteService clienteService;
 
-    @GetMapping("/clientes")
+    @GetMapping("/cliente")
     public List<Cliente> index(){
         return clienteService.findAll();
     }
 
-    @GetMapping("/clientes/{id}")
-    public Cliente show(@PathVariable Long id){
-        return clienteService.findBy(id);
+    @GetMapping("/cliente/{id}")
+    public ResponseEntity<?> show(@PathVariable Long id) {
+        Cliente cliente = clienteService.findBy(id);
+        Map<String, Object> response = new HashMap<>();
+        try{
+
+        }catch (DataAccessException da){
+            response.put("mensaje", "Error al realizar la consulta en la base de datos");
+            response.put("error", da.getMessage().concat(": ").concat(da.getMostSpecificCause().getMessage()));
+            return new ResponseEntity<Map<String, Object>>(response, HttpStatus.NOT_FOUND);
+        }
+        if (cliente == null){
+            response.put("mensaje", "El cliente ID: ".concat(id.toString().concat(" no existe en la base de datos")));
+            return new ResponseEntity<Map<String, Object>>(response, HttpStatus.NOT_FOUND);
+        }
+        return new ResponseEntity<Cliente>(cliente, HttpStatus.OK);
     }
 
-    @PostMapping("/clientes")
+    @PostMapping("/cliente")
     @ResponseStatus(HttpStatus.CREATED)
     public Cliente create(@RequestBody Cliente cliente){
         return clienteService.save(cliente);
     }
 
-    @PutMapping("/clientes/{id}")
+    @PutMapping("/cliente/{id}")
     @ResponseStatus(HttpStatus.CREATED)
     public Cliente update(@RequestBody Cliente cliente, @PathVariable Long id){
         Cliente clienteActual = clienteService.findBy(id);
@@ -42,7 +59,7 @@ public class ClienteRestController {
         return clienteService.save(clienteActual);
     }
 
-    @DeleteMapping("/clientes/{id}")
+    @DeleteMapping("/cliente/{id}")
     @ResponseStatus(HttpStatus.NO_CONTENT)
     public void delete(@PathVariable Long id){
         clienteService.delete(id);
